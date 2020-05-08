6defe2e2d0ebcae9809ed398f7c2e3f4ddc6ea69
prasad,srinivas <Srinivas.Prasad1@ge.com>
2020-05-08 22:42:15 +0530

    serve web page

--- 7-webServer-express-Js/server.js
+++ 7-webServer-express-Js/server.js
@@ -6,7 +6,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var path = require('path');
 
+
+//simple get
 app.get('/', (req, res) => res.send('What’s my destiny, Mama?'))
+
+
+//serving html
+app.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+//serving assets
+app.use('/assets',express.static(__dirname + '/assets/'));
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
